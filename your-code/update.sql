3f2c6bece28869be77467922cf155debdc52b642
Jennifer Atance <jenatance@gmail.com>
2022-06-13 20:30:32 +0200

    bonus

---
+++ your-code/update.sql
@@ -0,0 +1,11 @@
+UPDATE Customers
+SET email = 'ppicasso@gmail.com'
+WHERE name_ = 'Pablo Picasso';
+
+UPDATE Customers
+SET email = 'lamarr_hd@hollywood.com'
+WHERE name_ = 'Hedy Lamarr';
+
+UPDATE Customers
+SET email = 'hello@nasa.gov'
+WHERE name_ = 'Katherine Johnson';
