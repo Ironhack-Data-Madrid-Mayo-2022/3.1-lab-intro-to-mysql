732a52ea9a216a1b5bf1502e669086548643e448
Alex <alex.dominguez.arquero@gmail.com>
2022-06-14 08:55:47 +0200

    Lab SQL

---
+++ yourcode/seeding.sql
@@ -0,0 +1,15 @@
+insert into cars (VIN, manufacturer, model, year, color)
+values ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+		('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+        ('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+        ('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4',	2018, 'Silver'),
+		('DAM41UDN3CHU2WVF6', 'Volvo',  'V60', 2019, 'Gray'),
+        ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+
+
+
+SELECT * FROM customers;
+
+SELECT * FROM salespersons;
+
+SELECT * FROM invoices;
